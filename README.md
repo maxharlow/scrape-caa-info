0584c111eddffba342fcb088f4eafd9ffcaf9f66
Max Harlow <max.harlow@os.uk>
2017-10-11 18:52:31 +0100

    Modernise, update for site changes

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 Scrape CAA Info
 ===============
 
-The [Civil Aviation Authority (CAA)] (http://www.caa.co.uk/) publish their 'G-INFO' [database of every registered aircraft] (http://www.caa.co.uk/application.aspx?catid=60&pagetype=65&appid=1). This scrapes those records into a CSV.
+The [Civil Aviation Authority (CAA)](https://www.caa.co.uk/) publish their 'G-Info' [database of every registered aircraft](http://publicapps.caa.co.uk/modalapplication.aspx?appid=1). This scrapes those records into a CSV.
 
-Requires a recent version of [Node JS] (http://nodejs.org/).
+Requires a recent version of [Node](https://nodejs.org/).
 
 Install the dependencies with `npm install`, then run `node caa-info`.
--- caa-info.js
+++ caa-info.js
@@ -1,59 +1,70 @@
-var highland = require('highland')
-var request = require('request')
-var cheerio = require('cheerio')
-var fs = require('fs')
-var csvWriter = require('csv-write-stream')
+const Highland = require('highland')
+const Request = require('request')
+const RetryMe = require('retry-me')
+const Cheerio = require('cheerio')
+const FS = require('fs')
+const CSVWriter = require('csv-write-stream')
 
-var http = highland.wrapCallback(function (location, callback) {
-    request(location, function (error, response, body) {
-	var errorStatus = (response.statusCode >= 400) ? new Error(response.statusCode) : null
-	response.body = body
-	callback(error || errorStatus, response)
-    })
+const http = Highland.wrapCallback((location, callback) => {
+    const input = output => {
+        Request(location, (error, response) => {
+            const failure = error ? error : (response.statusCode >= 400) ? new Error(response.statusCode) : null
+            output(failure, response)
+        })
+    }
+    const times = {
+        retries: -1,
+        factor: 1.25,
+        maxTimeout: 5 * 60 * 1000
+    }
+    RetryMe(input, times, callback)
 })
 
-console.log('Retrieving info...')
-
-var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
-var regmarks = alphabet.map(function (x) { return alphabet.map(function (y) { return x + y }) }).join(',').split(',')
-
-function locate(regmark) {
-    return 'http://www.caa.co.uk/application.aspx?catid=60&pagetype=65&appid=1&mode=summary&regmark=' + regmark
+function regmarks() {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  const regmarks = alphabet.map(x => alphabet.map(y => x + y)).join(',').split(',')
+  return regmarks.map(regmark => {
+      return {
+          uri: 'http://publicapps.caa.co.uk/modalapplication.aspx?appid=1&mode=summary&regmark=' + regmark,
+          regmark
+      }
+  })
 }
 
-function scrape(response) {
-    var document = cheerio.load(response.body)
-    var items = document('.items').text().match(/Showing 1 to 20 of (.*) items/)
-    var count = items ? items[1] : 0 // the text won't appear if the regmark is invalid, so skip
-    var links = []
-    for (var i = 0; i < count; i++) {
-	links.push(response.request.href.replace('summary', 'detail') + '&dataindex=' + i)
-    }
-    return links
+function pages(response) {
+    const document = Cheerio.load(response.body)
+    const items = document('.items').text().match(/Showing 1 to 20 of (.+) items/)
+    const count = items ? Number(items[1]) : 0 // the text won't appear if the regmark is invalid, so skip
+    console.log('Scraping ' + count + ' registrations beginning with ' + response.request.regmark.toUpperCase() + '...')
+    return Array(count).fill().map((_, i) => {
+        return response.request.href.replace('summary', 'detail') + '&dataindex=' + i
+    })
 }
 
 function results(response) {
-    var document = cheerio.load(response.body)
+    const document = Cheerio.load(response.body)
+    const registrationMark = document('#currentModule_currentModule_Registration').text().trim()
+    process.stdout.write(registrationMark + ', ')
     return {
-	registrationMark: document('#currentModule_currentModule_Registration').html(),
-	registrationPreviousId: document('#currentModule_currentModule_PreviousID').html(),
-	registrationStatus: document('#currentModule_currentModule_Status').html(),
-	registrationStatusTo: document('#currentModule_currentModule_ToLabel').html(),
-	registrationStatusReason: document('#currentModule_currentModule_Reason').html(),
-	registrationCurrentDate: document('#currentModule_currentModule_CurrentRegDate').html(),
-	registrationDeregistrationDate: document('#currentModule_currentModule_DeRegDate').html(),
-	aircraftManufacturer: document('#currentModule_currentModule_Manufacturer').html(),
-	aircraftType: document('#currentModule_currentModule_Type').html(),
-	ownerStatus: document('#currentModule_currentModule_OwnershipStatus').html(),
-	ownerRegistration: document('#currentModule_currentModule_RegisteredOwners').html().replace(/<br>/g, '\n')
+        registrationMark,
+        registrationPreviousId: document('#currentModule_currentModule_PreviousID').text().trim(),
+        registrationStatus: document('#currentModule_currentModule_Status').text().trim(),
+        registrationStatusTo: document('#currentModule_currentModule_ToLabel').text().trim(),
+        registrationStatusReason: document('#currentModule_currentModule_Reason').text().trim(),
+        registrationCurrentDate: document('#currentModule_currentModule_CurrentRegDate').text().trim(),
+        registrationDeregistrationDate: document('#currentModule_currentModule_DeRegDate').text().trim(),
+        aircraftManufacturer: document('#currentModule_currentModule_Manufacturer').text().trim(),
+        aircraftType: document('#currentModule_currentModule_Type').text().trim(),
+        ownerStatus: document('#currentModule_currentModule_OwnershipStatus').text().trim(),
+        ownerRegistration: document('#currentModule_currentModule_RegisteredOwners').html().replace(/<br>/g, '\n').trim()
     }
 }
 
-highland(regmarks)
-    .map(locate)
+Highland(regmarks())
     .flatMap(http)
-    .flatMap(scrape)
+    .flatMap(pages)
     .flatMap(http)
     .map(results)
-    .through(csvWriter())
-    .pipe(fs.createWriteStream('caa-info.csv'))
+    .errors(e => console.error(e.stack))
+    .through(CSVWriter())
+    .pipe(FS.createWriteStream('caa-info.csv'))
